62b3d8ca06fd707d6885a3c10b33ba7c6a4285b2
janupasilva2005 <janupasilva2005@gmail.com>
2023-06-10 12:40:17 +0530

    Basic painting

--- script.js
+++ script.js
@@ -4,40 +4,33 @@ const context = canvas.getContext("2d");
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
-let isMouseDowned = false;
+let isMouseDown;
+let mouse = {
+  x: undefined,
+  y: undefined,
+};
 
-class Mouse {
-  constructor(initialX, initialY) {
-    this.x = initialX;
-    this.y = initialY;
-  }
+const drawCircle = () => {
+  context.fillStyle = "blue";
 
-  update(x, y) {
-    this.x = x;
-    this.y = y;
-  }
-}
-const mousePosition = new Mouse(canvas.width / 2, canvas.height / 2);
+  context.beginPath();
+  context.arc(mouse.x, mouse.y, 3, 0, Math.PI * 2);
+  context.fill();
+};
 
-class Particle {
-  constructor(x, y, radius, color) {
-    this.x = x;
-    this.y = y;
-    this.radius = radius;
-    this.color = color;
+canvas.addEventListener("mousemove", (e) => {
+  mouse.x = e.clientX;
+  mouse.y = e.clientY;
+
+  if (isMouseDown) {
+    drawCircle();
   }
-}
+});
 
 canvas.addEventListener("mousedown", (e) => {
-  isMouseDowned = true;
+  isMouseDown = true;
 });
 
 canvas.addEventListener("mouseup", (e) => {
-  isMouseDowned = false;
-});
-
-canvas.addEventListener("mousemove", (e) => {
-  if (isMouseDowned) {
-    mousePosition.update(e.clientX, e.clientY);
-  }
+  isMouseDown = false;
 });
